f1d2f34f891a954f60753750579ba8c639d328d5
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-06 16:01:55 +0600

    allCard section update

--- src/Components/ALlBooks/AllBooks.jsx
+++ src/Components/ALlBooks/AllBooks.jsx
@@ -1,6 +1,5 @@
 import React, { use } from 'react';
-import { BsListNested } from 'react-icons/bs';
-import { FaStarHalfAlt } from "react-icons/fa";
+import AllCards from '../../Ui/AllCards';
 
 const booksPromise = fetch('/booksData.json').then(res => res.json())
 
@@ -14,31 +13,7 @@ const AllBooks = () => {
             </div>
             <div className='grid grid-cols-1 md:grid-cols-3  gap-5'>
                 {
-                    books.map(book => <div key={book.bookId}>
-                        <div className='border border-[#13131315] rounded-2xl p-6'>
-                            <div className='bg-[#F3F3F3] rounded-2xl flex justify-center items-center py-8'>
-                                <img className='h-50' src={book.image} alt="" />
-                            </div>
-
-                            <div className='flex gap-5'>
-                                {book.tags.map(tag => <div>
-
-                                    <div className='py-2 my-5 px-5  bg-[#23BE0A10] text-[#23BE0A] rounded-full'>
-                                        {tag}
-                                    </div>
-
-                                </div>)}
-                            </div>
-
-                            <h1 className='text-[#131313] font-bold text-2xl playfair mb-4'>{book.bookName}</h1>
-                            <p className='font-medium text-[#13131380]'>{book.author}</p>
-                            <div className='border-t border-gray-400 opacity-30 my-4'></div>
-                            <div className=' flex justify-between'>
-                                <div className='text-[#13131380] font-medium'>{book.category}</div>
-                                <div className='text-[#13131380] font-medium flex gap-2 items-center'>{book.rating} <FaStarHalfAlt /></div>
-                            </div>
-                        </div>
-                    </div>)
+                    books.map(book => <AllCards book={book} />)
                 }
             </div>
         </div>
---
+++ src/Ui/AllCards.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BsListNested } from 'react-icons/bs';
+import { FaStarHalfAlt } from "react-icons/fa";
+const AllCards = ({book}) => {
+    return (
+        <div className='border border-[#13131315] rounded-2xl p-6'>
+            <div className='bg-[#F3F3F3] rounded-2xl flex justify-center items-center py-8'>
+                <img className='h-50' src={book.image} alt="" />
+            </div>
+
+            <div className='flex gap-5'>
+                {book.tags.map(tag => <div>
+
+                    <div className='py-2 my-5 px-5  bg-[#23BE0A10] text-[#23BE0A] rounded-full'>
+                        {tag}
+                    </div>
+
+                </div>)}
+            </div>
+
+            <h1 className='text-[#131313] font-bold text-2xl playfair mb-4'>{book.bookName}</h1>
+            <p className='font-medium text-[#13131380]'>{book.author}</p>
+            <div className='border-t border-gray-400 opacity-30 my-4'></div>
+            <div className=' flex justify-between'>
+                <div className='text-[#13131380] font-medium'>{book.category}</div>
+                <div className='text-[#13131380] font-medium flex gap-2 items-center'>{book.rating} <FaStarHalfAlt /></div>
+            </div>
+        </div>
+    );
+};
+
+export default AllCards;
